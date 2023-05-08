9f2c65bc3bd9378787a407d5dc07bfdbe0a1d404
ats527 <truespiritz527@gmail.com>
2023-05-08 16:29:44 +0530

    added cors

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
   origin: ["http://127.0.0.1:3000", "http://localhost:3000", "https://server.ashahotel.in", "https://ashahotel.in"],
+  credentials: true
 }));
 
 app.use("/api/v2", managerRouter);
